492299a10cc266d7964fbc59f5ee45bca46cfc8a
kongchenglc <kongchenglc@gmail.com>
2024-10-24 11:14:05 -0400

    feat: article add image link

--- models/article.js
+++ models/article.js
@@ -4,6 +4,7 @@ const ArticleSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: String, required: true },
+  imageLink: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
 
